ec06329d04b260a2cb5ea98edd83d511ffb04273
Ramapriya Doom <roman@bx24-store.ru>
2020-02-22 17:08:33 +0700

    end of test handler, commit before move to prod

--- app/api/app.php
+++ app/api/app.php
@@ -1,3 +1,63 @@
-<?php  require __DIR__ . '/app/classes/crest.php';
+<?php
 
-Debug(CRest::call('placement.get'));
+require __DIR__ . '/app/classes/crest.php';
+require_once __DIR__.'/app/configs/db.php';
+
+$DB = new DB();
+
+$jsonRawData = file_get_contents('php://input');
+$jsonDecodedData = json_decode($jsonRawData);
+
+if(!empty($jsonDecodedData)) {
+    $count = intval($jsonDecodedData->count);
+    $diameter = intval($jsonDecodedData->diameter);
+    $width = intval($jsonDecodedData->width);
+    $materialType = intval($jsonDecodedData->materialType);
+    $paymentType = htmlspecialchars($jsonDecodedData->paymentType);
+    $transportCost = intval($jsonDecodedData->transportCost);
+
+    $raisingFactor = 1.10;
+
+    $result = [];
+    $result['result'] = 'success';
+    $result['holeCount'] = $count;
+    $result['materialType'] = $materialType;
+    $result['paymentType'] = $paymentType;
+    if($diameter > 350) {
+        $result['finalPrice'] = 'договорная цена';
+        echo json_encode($result, JSON_UNESCAPED_UNICODE);
+
+    } else {
+        $startDiameterSql = "SELECT * FROM DIAMETERS WHERE start_diameter <= $diameter ORDER BY start_diameter DESC LIMIT 1";
+        $startDiameterQuery = $DB->customSelect($startDiameterSql);
+        $startDiameterId = intval($startDiameterQuery[0]['id']);
+
+        $diameterPriceSql = "SELECT price FROM PRICES WHERE diameter_id = $startDiameterId && material_type_id = $materialType";
+
+        $diameterPriceQuery = $DB->customSelect($diameterPriceSql);
+        $diameterPrice = intval($diameterPriceQuery[0]['price']);
+        if($paymentType === 'wire') {
+            $diameterPrice = (float)(number_format(($diameterPrice * $raisingFactor), 2, '.',''));
+        }
+        $holePrice = $diameterPrice * $width;
+
+        if($holePrice < 800) {
+            $holePrice = 800;
+        }
+        
+        $finalPrice = ($holePrice * $count) + $transportCost;
+
+        if($finalPrice < 5000) {
+            $finalPrice = 5000;
+        }
+
+        $result['startDiameterId'] = $startDiameterId;
+        $result['startDiameterPrice'] = $diameterPrice;
+        $result['oneHolePrice'] = $holePrice;
+        $result['finalPrice'] = $finalPrice;
+
+        echo json_encode($result, JSON_UNESCAPED_UNICODE);
+    }
+
+    
+}
--- app/assets/app.js
+++ app/assets/app.js
@@ -1,17 +1,33 @@
-const app = new Vue({
-   el: '#app',
-   data: {
-      deal: '',
-      dealId: null
-   },
-   methods: {
-
-   },
-   created: function() {
-      
-   },
-   updated : function () {
-      
-      // BX24.resizeWindow(document.body.clientWidth, document.getElementsByClassName("workarea")[0].clientHeight);
-  },
-}) 
+window.onload = function() {
+   const formValues = {}
+   const form = document.querySelectorAll('.formData');
+   form.forEach(item => {
+       const elem = document.getElementById(item.id);
+       elem.addEventListener('change', function() {
+           switch(item.type) {
+               case 'number':
+               case 'select-one':
+                   formValues[elem.name] = elem.value
+                   break;
+               case 'checkbox':
+                   formValues[elem.name] = elem.checked
+                   break;
+           }
+       });
+   });
+   const btn = document.querySelector('button');
+   btn.addEventListener('click', () => {
+       const CalcPrice = new Promise((resolve, reject) => {
+           fetch('tst.fetch.php', {
+               method: 'post',
+               body: JSON.stringify(formValues)
+           })
+           .then(response => {
+               resolve(response.json())
+           })
+       })
+       CalcPrice.then(data => {
+           console.log(data)
+       })
+   })
+}
